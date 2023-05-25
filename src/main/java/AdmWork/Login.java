8aaf5318dd3988da47dc6d1c3c8a4f7a808ea0c8
andryfall <andryfall@student.telkomuniversity.ac.id>
2023-05-25 18:19:49 +0700

    Login()

--- src/main/java/AdmWork/Login.java
+++ src/main/java/AdmWork/Login.java
@@ -4,10 +4,39 @@
  */
 package AdmWork;
 
+import DBPr.KumpulanAkunAdmin;
+
 /**
  *
  * @author Iqro Banyuanto
  */
 public class Login {
+    public String id;
+    public String password;
     
+//    Cara pakainya panggil dulu getAkun sebelum verifikasiAkun sama jangan lupa dibuatkan dulu objek Login.
+//    Login l = new Login();
+//    l.getAkun([id akun], [password akun]);
+//    l.verifikasiAkun();
+    public Login(){
+    }
+    
+    public void getAkun(String id, String password){
+        this.id = id;
+        this.password = password;
+    }
+    
+    public void verifikasiAkun(){
+        
+        KumpulanAkunAdmin k = new KumpulanAkunAdmin();
+        try{
+            if(k.getAkun(id, password) == null){
+                throw new Exception("Id atau password salah");
+            }
+//          Nanti isi kode dibawah ini, apa yang terjadi kalau id dan password BENAR
+            System.out.println("Benar");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
