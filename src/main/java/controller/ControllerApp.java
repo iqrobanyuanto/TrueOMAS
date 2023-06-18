84e45406514a3d5f6a8318e5d67c887815fad9a7
MaximusBayu <maximusbayu9b16@gmail.com>
2023-06-18 15:12:02 +0700

    Membenarkan list pada jList

--- src/main/java/controller/ControllerApp.java
+++ src/main/java/controller/ControllerApp.java
@@ -69,47 +69,42 @@ public class ControllerApp {
     
  //#################################################################### ASSIGN #####################################################################
     //Untuk show GUI Assign-------
-    public static void ShowAssign(){
+    public void ShowAssign(){
         assign.setVisible(true);
         assign.setLocationRelativeTo(null);
         //refreshAssign();
     }
     
     //Mengupdate list pada Assign lembur
-//    public void refreshAssign() {
-//        listPgw = DaoEmp.getAllPegawai();
-//        framePegawai.getListModel().removeAllElements();
-//        for (Pegawai pgw : listPgw) {
-//            if(pgw.statusLembur.getStatusTiket() == true){
-//                framePegawai.getListModel().addElement(pgw.getNamaEmployee());
-//            }
-//        }
-//        updateFormAssign();
-//    }
-    
-    //Deskripsi pada list yang dituju
-    public void AssignDesc(){
-        
+    public void refreshAssign(JList jlist) {
+        DefaultListModel model = new DefaultListModel();
+        listPgw = DaoEmp.getAllPegawai();
+        for (Pegawai pgw : listPgw) {
+            if(pgw.statusLembur.getStatusTiket() == true){
+                model.addElement(pgw.getNamaEmployee());
+            }
+        }
+        jlist.setModel(model);
     }
     
     //update list pegawai di GUI Assign
-//    public void updateFormAssign() {
-//        int jamLembur = pegawai.statusLembur.getWaktuLembur();
-//        String sJamLembur = Integer.toString(jamLembur); // Convert int to String
-//        var pengizin = pegawai.statusLembur.getPengizin();
-//        var selectedIndex = assign.getjList1().getSelectedIndex();
-//        if (selectedIndex >= 0 && selectedIndex < listPgw.size()) {
-//            Pegawai pgw = listPgw.get(selectedIndex);
-//            assign.getjLabel4().setText(pgw.getNamaEmployee());
-//            assign.getjLabel6().setText(pengizin);
-//            assign.getjLabel8().setText(sJamLembur);
-//        } else {
-//            assign.getjLabel4().setText("");
-//            assign.getjLabel6().setText("");
-//            assign.getjLabel8().setText("");
-//        }
-//        
-//    }
+    public void updateFormAssign() {
+        int jamLembur = pegawai.statusLembur.getWaktuLembur();
+        String sJamLembur = Integer.toString(jamLembur); // Convert int to String
+        var pengizin = pegawai.statusLembur.getPengizin();
+        int selectedIndex = assign.getjList1().getSelectedIndex();
+        if (selectedIndex >= 0 && selectedIndex < listPgw.size()) {
+            Pegawai pgw = listPgw.get(selectedIndex);
+            assign.getjLabel4().setText(pgw.getNamaEmployee());
+            assign.getjLabel6().setText(pengizin);
+            assign.getjLabel8().setText(sJamLembur);
+        } else {
+            assign.getjLabel4().setText("");
+            assign.getjLabel6().setText("");
+            assign.getjLabel8().setText("");
+        }
+        
+    }
     
  //###############################################################################################################################################
     
