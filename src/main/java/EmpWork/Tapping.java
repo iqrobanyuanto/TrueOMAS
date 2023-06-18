77aa42d2b87fe4497fe3c272048ffb1fb4fb4494
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-06-18 17:01:30 +0700

    Revert "Merge branch 'main' into GUILIyanFINALplz"
    This reverts commit 6620f7f4469f0cb2bef387afaa8a7ed030c3ffcb, reversing
    changes made to 4f7d284e7293f48836cc276ccb135e9a39cfe78c.

--- src/main/java/EmpWork/Tapping.java
+++ src/main/java/EmpWork/Tapping.java
@@ -3,33 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package EmpWork;
-import Dao.KehadiranDao;
-import Dao.RecordKehadiranDao;
+import DBPr.KumpulanEmployee;
 /**
  *
  * @author Iqro Banyuanto
  */
 public class Tapping {
-    public static void Tap(Employee q){
+    public static void Tap(String namaJabatan, String idEmployee){
         try{
+            KumpulanEmployee r = new KumpulanEmployee();
+            Employee q = r.searchEmployee(namaJabatan, idEmployee);
             if(q == null){
                 throw new IllegalArgumentException("Employee tidak ditemukan");
             }
-            KehadiranDao DBkehadiran = new KehadiranDao();
             if(q.kartuKehadiran.cekJamMasuk() == -99){
-                q.kartuKehadiran.setTappingWaktuMasuk();
-                DBkehadiran.updateKartuKehadiran(q.kartuKehadiran, q.getIdEmployee(), q.getNamaJabatan());
+                q.kartuKehadiran.setWaktuMasuk();
+                if(q.kartuKehadiran.getWaktu_masuk() != null){
+                    q.list_kehadiran.add(q.kartuKehadiran.getWaktu_masuk());
+                }
             }else{
-                q.kartuKehadiran.setTappingWaktuKeluar();
-                RecordKehadiran e = new RecordKehadiran();
-                e.setTanggal(q.kartuKehadiran.getWaktu_masuk().toLocalDate());
-                e.setWaktuMasuk(q.kartuKehadiran.getWaktu_masuk().toLocalTime());
-                e.setWaktuKeluar(q.kartuKehadiran.getWaktu_keluar().toLocalTime());
-                RecordKehadiranDao a = new RecordKehadiranDao();
-                a.insertLog(e, q);
+                q.kartuKehadiran.setWaktuKeluar();
+                q.list_kehadiran.add(q.kartuKehadiran.getWaktu_keluar());
                 prosesTambahTotalJamKerja(q);
-                DBkehadiran.updateKartuKehadiran(q.kartuKehadiran, q.getIdEmployee(), q.getNamaJabatan());
-                
             }
         }catch(IllegalArgumentException e){
             System.out.println(e.getMessage());
